19584510166c5bdda310f5402b349429bdb8582f
calixte <calixteairreymond@gmail.com>
2024-03-13 09:43:33 +0100

    habit model

--- Models/Habit.swift
+++ Models/Habit.swift
@@ -16,7 +16,13 @@ enum Status: String, CaseIterable{
 
 enum Day: String, CaseIterable{
     case Monday = "MON"
-    case Models
+    case Tuesday = "TUE"
+    case Wednesday = "WED"
+    case Thursday = "THU"
+    case Friday = "FRI"
+    case Saturday = "SAT"
+    case Sunday = "SUN"
+    case all = "all"
 }
 
 
@@ -24,14 +30,20 @@ struct Habit: Identifiable {
     var id = UUID()
     var name: String
     var noftification: Bool
+    var timesheet: String
     var quantity: Int
     var quantityDone: Int
     var status: Status
     var streak: Int
+    var repetition: [Day] = []
+
     
     
     
-    static var Habit = [
-        Habit(name: "call prarent")
+    static var habitData = [
+        Habit(name: "call prarent",noftification: true,timesheet:"12h32", quantity: 1,quantityDone: 0,status: .toDo, streak: 5, repetition: [.all]),
+        Habit(name: "call dog",noftification: false,timesheet:"12h32", quantity: 3,quantityDone: 2,status: .toDo, streak: 0, repetition: [.Friday,.Monday]),
+        Habit(name: "run",noftification: false,timesheet:"12h32", quantity: 1,quantityDone: 1,status: .done, streak: 10, repetition: [.all])
+        
     ]
 }
