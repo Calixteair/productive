002c3ab8e9b001ee7476851e6c14e05895a8d08c
calixte <calixteairreymond@gmail.com>
2024-03-13 09:30:05 +0100

    debut habit model

--- Models/Habit.swift
+++ Models/Habit.swift
@@ -8,7 +8,30 @@
 import Foundation
 
 
+enum Status: String, CaseIterable{
+    case done = "done"
+    case toDo = "todo"
+    case suspend = "suspend"
+}
+
+enum Day: String, CaseIterable{
+    case Monday = "MON"
+    case Models
+}
+
+
 struct Habit: Identifiable {
     var id = UUID()
-    var name
+    var name: String
+    var noftification: Bool
+    var quantity: Int
+    var quantityDone: Int
+    var status: Status
+    var streak: Int
+    
+    
+    
+    static var Habit = [
+        Habit(name: "call prarent")
+    ]
 }
