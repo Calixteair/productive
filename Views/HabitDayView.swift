b3b376e20afacf14c5a36b676b5e5075ada45bfc
calixte <calixteairreymond@gmail.com>
2024-03-13 09:54:02 +0100

    début view

---
+++ Views/HabitDayView.swift
@@ -0,0 +1,22 @@
+//
+//  HabitDayView.swift
+//  Projethabit
+//
+//  Created by reymond calixte on 13/03/2024.
+//
+
+import SwiftUI
+
+
+
+struct HabitDayView: View {
+    var body: some View {
+        NavigationView{
+            
+}
+
+struct HabitDayView_Previews: PreviewProvider {
+    static var previews: some View {
+        HabitDayView()
+    }
+}
