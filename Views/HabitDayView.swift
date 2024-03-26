0d62ec2496aad87ed336b241eecf060154282bc1
Zeky69 <zek.akburak@gmail.com>
2024-03-26 13:34:10 +0100

    popopo

--- Views/HabitDayView.swift
+++ Views/HabitDayView.swift
@@ -12,41 +12,115 @@ import SwiftUI
 struct HabitDayView: View {
     
     @EnvironmentObject var data: HabitViewModel
+    @Namespace var animation
     @State private var showAlert = false
     @State private var habitToDelete: Habit?
     
     
     var body: some View {
         NavigationView{
-            List {
-                ForEach(data.habits) {habit in
-                    if habit.status != .suspend {
-                        RowView(habit: habit, type: .task)
-                            .onTapGesture {
-                                data.completHabit(habit: habit)
+            
+            VStack{
+                ScrollView(.horizontal , showsIndicators: false){
+                    HStack(spacing: 10){
+                        ForEach(data.currentWeek,id: \.self){ day in
+                            
+                            VStack(spacing: 10){
+                                Text(data.extractDate(date: day, format: "dd"))
+                                Text(data.extractDate(date: day, format: "EEE"))
+                                    .font(.system(size:16))
+                                    .fontWeight(.semibold)
+                                
+                                Circle()
+                                    .fill(.white)
+                                    .frame(width: 8, height: 8)
+                                    .opacity(data.isToday(date: day) ? 1: 0)
+                                
+                                
                             }
+                            .frame(width: 45, height: 90)
+                            .foregroundStyle(data.isToday(date: day) ? .primary : .secondary)
+                            .background(
+                            
+                                ZStack{
+                                    if data.isToday(date: day){
+                                        Capsule()
+                                            .fill(Color(red: 1, green: 0.8470, blue: 0.8470))
+                                            .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
+                                            
+                                    }
+                                }
+                                
+                            )
+                            .contentShape(Capsule())
+                            .onTapGesture {
+                                withAnimation{
+                                    data.currentDay = day
+                                }
+                            }
+                                
+                            
+                        }
                     }
+                    .padding(.horizontal)
                 }
-                .onDelete { indexSet in
-                    if let firstIndex = indexSet.first {
-                        self.habitToDelete = data.habits[firstIndex]
-                        self.showAlert.toggle()
+                
+                
+                
+                List {
+                    
+                    
+                    if let tasks = data.filteredHabits{
+                        if tasks.isEmpty{
+                            Text("Aucune tache trouvé")
+                                .font(.system(size:16))
+                                .fontWeight(.light)
+                                .offset(y:100)
+                            
+                        }
+                        else{
+                            ForEach(tasks) {habit in
+                                if habit.status != .suspend {
+                                    RowView(habit: habit, type: .task)
+                                        .onTapGesture {
+                                            data.completHabit(habit: habit)
+                                        }
+                                        .listRowInsets(EdgeInsets())
+                                        .listRowSeparator(.hidden)
+                                        .padding(5)
+                                }
+                            }
+                            .onDelete { indexSet in
+                                if let firstIndex = indexSet.first {
+                                    self.habitToDelete = tasks[firstIndex]
+                                    self.showAlert.toggle()
+                                }
+                            }
+                            .onMove(perform: data.moveItem)
+                        }
+                        
                     }
+                   
                 }
-                .onMove(perform: data.moveItem)
-            }
-            .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("Confirmation"),
-                    message: Text("Are you sure you want to delete this habit?"),
-                    primaryButton: .destructive(Text("Delete")) {
-                        if let habitToDelete = habitToDelete {
-                            data.suspendHabit(habit: habitToDelete)
+                .listStyle(.plain)
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Confirmation"),
+                        message: Text("Are you sure you want to delete this habit?"),
+                        primaryButton: .destructive(Text("Delete")) {
+                            if let habitToDelete = habitToDelete {
+                                data.suspendHabit(habit: habitToDelete)
                             }
                             self.habitToDelete = nil
-                    },
-                    secondaryButton: .cancel()
-                )
+                        },
+                        secondaryButton: .cancel()
+                    )
+                }
+                .onChange(of: data.currentDay){ newValue in
+                    data.filterTodayHabits()
+                    
+                }
+            
             }
             .navigationTitle("Habit")
             .listStyle(PlainListStyle())
@@ -63,6 +137,9 @@ struct HabitDayView: View {
     }
 }
     
+
+
+                                                                                                                                                                                                                                        
     
     
     struct HabitDayView_Previews: PreviewProvider {
--- ViewsModels/HabitViewModel.swift
+++ ViewsModels/HabitViewModel.swift
@@ -12,14 +12,82 @@ class HabitViewModel: ObservableObject {
     
     @Published var habits: [Habit] = []
     
+    @Published var currentWeek: [Date] = []
+    
+    @Published var currentDay: Date = Date()
+    
+    @Published var filteredHabits: [Habit]?
+    
+    func fetchCurrentWeek(){
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let week = calendar.dateInterval(of: .weekOfMonth, for: today)
+        
+        guard let firstWeekDay = week?.start else{
+            return
+        }
+        
+        (1...7).forEach { day in
+            if let weekday = calendar.date( byAdding: .day , value: day , to:firstWeekDay ){
+                currentWeek.append(weekday)
+                
+            }
+            
+        }
+    }
+    
+    
+    
+    
+    
+    func extractDate(date:Date , format: String)-> String{
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = format
+        
+        return formatter.string(from: date)
+        
+    }
+    
+    
+    func isToday(date: Date)->Bool{
+        let calendar = Calendar.current
+        return calendar.isDate(currentDay, inSameDayAs: date)
+        
+    }
     
     init() {
         getHabits()
+        fetchCurrentWeek()
+        filterTodayHabits()
     }
     
     func getHabits(){
         habits.append(contentsOf: Habit.habitData)
     }
+    
+    func filterTodayHabits(){
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            
+            let calendar = Calendar.current
+            let filtered = self.habits.filter{
+                return calendar.isDate($0.timesheet,inSameDayAs: self.currentDay)
+            }
+            
+            DispatchQueue.main.async {
+                
+                    self.filteredHabits = filtered
+            
+                
+                
+            }
+        }
+        
+    }
+    
+    
     
     func completHabit(habit : Habit){
         for ( index, td) in habits.enumerated(){
