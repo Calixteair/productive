3739aceaa82399073ca3c996efc30d85910f9595
calixteair <113100230+Calixteair@users.noreply.github.com>
2024-04-02 14:00:59 +0200

    Update ListHistory.swift

--- Models/ListHistory.swift
+++ Models/ListHistory.swift
@@ -14,10 +14,46 @@ struct LisHistory: Identifiable {
     var lastDay: Date
     
     
-    static var listItems = [
-        ItemHistory(idHabit: Habit.habitData[0].id, streak:Habit.habitData[0].streak-2 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (4 * 24 * 60 * 60)), status: .done),
-        ItemHistory(idHabit: Habit.habitData[0].id, streak:Habit.habitData[0].streak-1 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (3 * 24 * 60 * 60)), status: .done)
-    ]
+        static var listItems = [
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:1, date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (56 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:2, date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (49 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:3 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (42 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:4 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (35 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:5 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (28 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:6 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (21 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:7 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (14 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:8 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (7 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[5].id, streak:9 , date: Date(), status: .done),
+        
+        ItemHistory(idHabit: Habit.habitData[4].id, streak:1 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (7 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[4].id, streak: 0 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (6 * 24 * 60 * 60)), status: .toDo),
+        ItemHistory(idHabit: Habit.habitData[4].id, streak:1 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (5 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[4].id, streak: 0 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (4 * 24 * 60 * 60)), status: .toDo),
+        ItemHistory(idHabit: Habit.habitData[4].id, streak:1 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (3 * 24 * 60 * 60)), status: .done),
+        
+        
+        ItemHistory(idHabit: Habit.habitData[3].id, streak: 1 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (8 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[3].id, streak: 2 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (7 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[3].id, streak: 3 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (6 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[3].id, streak: 4 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (5 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[3].id, streak: 5 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (4 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[3].id, streak: 6 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (3 * 24 * 60 * 60)), status: .done),
+        ItemHistory(idHabit: Habit.habitData[3].id, streak: 7 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (2 * 24 * 60 * 60)), status: .done),
+        
+        ItemHistory(idHabit: Habit.habitData[1].id, streak: 1 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (12 * 24 * 60 * 60)), status: .done),
+        
+        ItemHistory(idHabit: Habit.habitData[0].id, streak: 1 , date: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (1 * 24 * 60 * 60)), status: .toDo),
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ]
 
 
     static var histoData = LisHistory(items: listItems ,lastDay: Date(timeIntervalSince1970: Date().timeIntervalSince1970 - (2 * 24 * 60 * 60)))
